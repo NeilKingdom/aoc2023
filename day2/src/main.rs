1c6a4476556c5aa7aec64d613164b82adc06472c
NeilKingdom <neil@neilkingdom.xyz>
2023-12-02 22:13:42 -0500

    finished day2 part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,17 +5,27 @@ use std::fs::File;
 
 #[derive(Default, Debug, Copy, Clone)]
 struct CubeSet {
-    red_count: u8,
     blue_count: u8,
     green_count: u8,
+    red_count: u8,
 }
 
 impl CubeSet {
+    fn new(blue_count: u8, green_count: u8, red_count: u8) -> Self {
+        CubeSet { red_count, blue_count, green_count } 
+    }
+    
+    fn clear(&mut self) -> () {
+        self.blue_count = 0;
+        self.green_count = 0;
+        self.red_count = 0;
+    }
+
     fn inc_color_count(&mut self, num: &u8, color: &str) -> () {
         match color {
-            "red" => self.red_count += num,
             "blue" => self.blue_count += num,
             "green" => self.green_count += num,
+            "red" => self.red_count += num,
             _ => panic!("Unexpected color")
         }
     }
@@ -31,7 +41,7 @@ fn main() -> std::io::Result<()> {
     let reader = reader.lines().map(|x| {
         x.expect("Could not read line")
             .replacen(":", "", 1)
-            .replace(",", "")
+            .replace(",", "") + ";"
     });
 
     for line in reader {
@@ -62,13 +72,30 @@ fn main() -> std::io::Result<()> {
 
                 if eof {
                     cube_sets.push(cube_set);
+                    cube_set.clear();
                 }
             }
         }
 
-        println!("game_id: {game_id} {:#?}", cube_sets);
+        //println!("game_id: {game_id} {:#?}", cube_sets);
         games.push((game_id, cube_sets));
     }
+
+    //                            Red, blue, green
+    let actual_cubes = CubeSet::new(14, 13, 12);
+    let mut id_tally: u32 = 0;
+
+    for (game_id, cube_sets) in games {
+        if cube_sets.iter().all(|cube_set| {
+            cube_set.red_count <= actual_cubes.red_count &&
+            cube_set.blue_count <= actual_cubes.blue_count &&
+            cube_set.green_count <= actual_cubes.green_count
+        }) {
+            id_tally += game_id as u32;
+        }
+    }
+
+    println!("Answer: {id_tally}");
 
     Ok(())
 }
